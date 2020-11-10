bfb34c06b323ee39360bef5acd6722086eb99abf
Dmitriy Beglov <d.beglov@galament.com>
2020-11-10 21:54:34 +0400

    feat: create GlobalStyles

--- gatsby/src/components/Layout.tsx
+++ gatsby/src/components/Layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 import Nav from './Nav';
 import Footer from './Footer';
 
 const Layout: React.FC = (props) => {
     return (
         <div>
+            <GlobalStyles />
             <Nav />
             {props.children}
             <Footer />
