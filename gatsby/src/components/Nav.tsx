cda803b45d7d7d7399e5f466626bcc9a41042b8f
Dmitriy Beglov <d.beglov@galament.com>
2020-12-02 21:53:54 +0400

    feat: style Nav

--- gatsby/src/components/Nav.tsx
+++ gatsby/src/components/Nav.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+    margin-bottom: 3rem;
+
+    .logo {
+        transform: translate(-25%);
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        list-style: none;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr auto 1fr 1fr;
+        grid-gap: 2rem;
+        align-items: center;
+    }
+
+    li {
+        --rotate: -2deg;
+        transform: rotate(var(--rotate));
+        order: 1;
+
+        &:nth-child(1) {
+            --rotate: 1deg;
+        }
+
+        &:nth-child(2) {
+            --rotate: -2.5deg;
+        }
+
+        &:nth-child(4) {
+            --rotate: 2.5deg;
+        }
+
+        &:hover {
+            --rotate: 3deg;
+        }
+    }
+
+    a {
+        font-size: 3rem;
+        text-decoration: none;
+
+        &:hover {
+            color: var(--red);
+        }
+    }
+`;
 
 const Nav = () => {
     return (
-        <nav>
+        <NavStyles>
             <ul>
                 <li>
                     <Link to='/'>Hot Now</Link>
@@ -12,7 +65,9 @@ const Nav = () => {
                     <Link to='/pizzas'>Pizza Menu</Link>
                 </li>
                 <li>
-                    <Link to='/'>LOGO</Link>
+                    <Link to='/'>
+                        <Logo />
+                    </Link>
                 </li>
                 <li>
                     <Link to='/slicemasters'>SliceMasters</Link>
@@ -21,7 +76,7 @@ const Nav = () => {
                     <Link to='/order'>Order Ahead!</Link>
                 </li>
             </ul>
-        </nav>
+        </NavStyles>
     );
 };
 
