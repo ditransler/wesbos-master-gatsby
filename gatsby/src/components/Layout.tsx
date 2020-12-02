b77c4d33ea787bd9278a0e67dbd2aa8573b20533
Dmitriy Beglov <d.beglov@galament.com>
2020-12-02 21:37:01 +0400

    feat: create Typography

--- gatsby/src/components/Layout.tsx
+++ gatsby/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 import Nav from './Nav';
 import Footer from './Footer';
 
@@ -8,6 +9,7 @@ const Layout: React.FC = (props) => {
     return (
         <div>
             <GlobalStyles />
+            <Typography />
             <Nav />
             {props.children}
             <Footer />
