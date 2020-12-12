2bf58bb9da245ec37d70702ce84280531b08adeb
Dmitriy Beglov <d.beglov@galament.com>
2020-12-12 14:37:42 +0400

    feat: create topping schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,11 +5,12 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 // Then we give our schema to the builder and provide the result to Sanity
 
 import pizza from './pizza';
+import topping from './topping';
 
 export default createSchema({
     // We name our schema
     name: 'default',
     // Then proceed to concatenate our document type
     // to the ones provided by any plugins that are installed
-    types: schemaTypes.concat([pizza])
+    types: schemaTypes.concat([pizza, topping])
 });
