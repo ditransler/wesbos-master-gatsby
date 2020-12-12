90446e040aa9077038dd52c2fb083f281fb05f77
Dmitriy Beglov <d.beglov@galament.com>
2020-12-12 14:55:27 +0400

    feat: add pizza preview with toppings

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -38,6 +38,32 @@ export default {
             description: 'Price of the pizza in cents',
             validation: (Rule) => Rule.min(1000)
             // TODO: Add custom input component
+        },
+        {
+            name: 'toppings',
+            title: 'Toppings',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'topping' }] }]
         }
-    ]
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name'
+        },
+        prepare: ({ title, media, ...toppings }) => {
+            // 1. Filter undefined toppings out
+            const filteredToppings = Object.values(toppings).filter(Boolean);
+            // 2. Return the preview object for the pizza
+            return {
+                title,
+                media,
+                subtitle: filteredToppings.join(', ')
+            };
+        }
+    }
 };
