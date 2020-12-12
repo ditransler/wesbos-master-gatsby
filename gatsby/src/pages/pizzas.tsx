48015c467c1057527f8e64989eb8f1e84685a574
Dmitriy Beglov <d.beglov@galament.com>
2020-12-12 19:36:42 +0400

    feat: query and render pizzas

--- gatsby/src/pages/pizzas.tsx
+++ gatsby/src/pages/pizzas.tsx
@@ -1,7 +1,39 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-const PizzasPage = () => {
-    return <p>Pizzas page</p>;
+const PizzasPage = ({ data }) => {
+    const pizzas = data.pizzas.nodes;
+    return <PizzaList pizzas={pizzas} />;
 };
+
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes {
+                name
+                id
+                price
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+                image {
+                    asset {
+                        fixed(width: 600, height: 200) {
+                            ...GatsbySanityImageFixed
+                        }
+                        fluid(maxWidth: 400) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
 
 export default PizzasPage;
