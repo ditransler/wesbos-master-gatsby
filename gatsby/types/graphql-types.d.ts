2108523cd1532fa12f5a8d5167885474e8b68693
Dmitriy Beglov <d.beglov@galament.com>
2020-12-13 11:20:24 +0400

    feat: create graphql-types

---
+++ gatsby/types/graphql-types.d.ts
@@ -0,0 +1,48 @@
+export type Topping = {
+    id: string;
+    name: string;
+};
+
+type FixedImage = {
+    base64: string;
+    height: number;
+    width: number;
+    src: string;
+    srcSet: string;
+    srcSetWebp: string;
+    srcWebp: string;
+};
+
+type FluidImage = {
+    aspectRatio: number;
+    base64: string;
+    sizes: string;
+    src: string;
+    srcSet: string;
+    srcSetWebp: string;
+    srcWebp: string;
+};
+
+type ImageAsset = {
+    fixed: FixedImage;
+    fluid: FluidImage
+}
+
+export type PizzaNode = {
+    name: string;
+    id: string;
+    price: string;
+    slug: {
+        current: string;
+    };
+    toppings: Array<Topping>;
+    image: {
+        asset: ImageAsset;
+    };
+};
+
+export type PizzasQuery = {
+    pizzas: {
+        nodes: Array<PizzaNode>;
+    };
+};
