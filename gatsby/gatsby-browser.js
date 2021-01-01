bea160cac0b93a20ac0091b63054da9c9f64ff13
Dmitriy Beglov <d.beglov@galament.com>
2021-01-01 20:09:51 +0400

    feat(gatsby-browser&gatsby-ssr): wrap root element in OrderProvider

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Layout from './src/components/Layout';
+import { OrderProvider } from './src/contexts/OrderContext';
 
 export const wrapPageElement = ({ element, props }) => {
     return <Layout {...props}>{element}</Layout>;
 };
+
+export function wrapRootElement({ element }) {
+    return <OrderProvider>{element}</OrderProvider>;
+}
