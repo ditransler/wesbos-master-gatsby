a9854ef90ab50ddee77ad31a9f5b92ed97f5646d
Dmitriy Beglov <d.beglov@galament.com>
2021-01-04 11:03:03 +0400

    feat: add CurrentSlicing and HotSlices on index

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+const CurrentlySlicing = ({ slicemasters }) => {
+    return (
+        <div>
+            {!slicemasters && <LoadingGrid count={4} />}
+            {slicemasters && !slicemasters?.length && <p>No one is working right now!</p>}
+        </div>
+    );
+};
+
+const HotSlices = ({ hotSlices }) => {
+    return (
+        <div>
+            {!hotSlices && <LoadingGrid count={4} />}
+            {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
+        </div>
+    );
+};
 
 const HomePage = () => {
-    return <p>Home page</p>;
+    const { slicemasters, hotSlices } = useLatestData();
+
+    return (
+        <div className='center'>
+            <h1>The Best Pizza Downtown!</h1>
+            <p>Open 11am to 11pm Every Single Day</p>
+            <HomePageGrid>
+                <CurrentlySlicing slicemasters={slicemasters} />
+                <HotSlices hotSlices={hotSlices} />
+            </HomePageGrid>
+        </div>
+    );
 };
 
 export default HomePage;
