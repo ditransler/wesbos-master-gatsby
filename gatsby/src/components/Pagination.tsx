d40e9d66f111bf5023f4a736646c98c6d2ca14b7
Dmitriy Beglov <d.beglov@galament.com>
2021-01-06 11:23:35 +0400

    refactor(Pagination): update styles (add responsiveness)

--- gatsby/src/components/Pagination.tsx
+++ gatsby/src/components/Pagination.tsx
@@ -28,6 +28,14 @@ const PaginationStyles = styled.div`
             color: var(--grey);
         }
     }
+
+    @media (max-width: 800px) {
+        font-size: 1.4rem;
+
+        .word {
+            display: none;
+        }
+    }
 `;
 
 type PaginationProps = {
@@ -48,20 +56,20 @@ const Pagination: React.FC<PaginationProps> = ({ pageSize, totalCount, currentPa
 
     return (
         <PaginationStyles>
-            <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
-                ← Prev
+            <Link title='Prev Page' disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+                ← <span className='word'>Prev</span>
             </Link>
             {Array.from({ length: totalPages }).map((_, i) => (
                 <Link
                     className={currentPage === 1 && i === 0 ? 'current' : ''}
                     to={`${base}/${i > 0 ? i + 1 : ''}`}
-                    key={i}
+                    key={`page${i}`}
                 >
                     {i + 1}
                 </Link>
             ))}
-            <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
-                Next →
+            <Link title='Next Page' disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+                <span className='word'>Next</span> →
             </Link>
         </PaginationStyles>
     );
